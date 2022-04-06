a7db9896bff79343143956f3a55193ba32e0189f
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-06 11:21:43 -0500

    Toggle now fully functional
    Add toggle and different cards to index.js, now functional.

--- index.js
+++ index.js
@@ -16,130 +16,145 @@ navItems.forEach((navItem) => {
   });
 });
 
-const buttons = {
-  langs: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  other: [['See Live', ''], ['See Source', '']]
-}
-
-let cardsArr = [
+const cardsArr = [
   {
     name: 'card1',
     image: 'Icons/img-placeholder2.png',
     title: 'Professional Art Printing Data',
     text: 'A daily selection of privately personalized reads; no accounts or sign-ups required; has been the industry\'s standard',
-    link: {'project': '', 'source': ''}
+    link: { project: '', source: '' },
+    buttons: {
+      langs: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+      other: [['See Live', ''], ['See Source', '']],
+    },
   },
   {
     name: 'card2',
     image: 'Icons/img-placeholder2.1.png',
     title: 'Object-Oriented Programming',
     text: 'A daily selection of privately personalized reads; no accounts or sign-ups required; has been the industry\'s standard',
-    link: {'project': '', 'source': ''}
+    link: { project: '', source: '' },
+    buttons: {
+      langs: ['CSS', 'Bootstrap', 'Ruby on Rails'],
+      other: [['See Live', ''], ['See Source', '']],
+    },
   },
   {
     name: 'card3',
     image: 'Icons/img-placeholder1.png',
     title: 'Obsess No More With Your HTML',
     text: 'A daily selection of privately personalized reads; no accounts or sign-ups required; has been the industry\'s standard',
-    link: {'project': '', 'source': ''}
+    link: { project: '', source: '' },
+    buttons: {
+      langs: ['python', 'Bootstrap', 'Ruby on Rails'],
+      other: [['See Live', ''], ['See Source', '']],
+    },
   },
   {
     name: 'card4',
     image: 'Icons/img-placeholder2.1.png',
     title: 'Ruby Just Got Crazy',
     text: 'A daily selection of privately personalized reads; no accounts or sign-ups required; has been the industry\'s standard',
-    link: {'project': '', 'source': ''}
+    link: { project: '', source: '' },
+    buttons: {
+      langs: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+      other: [['See Live', ''], ['See Source', '']],
+    },
   },
   {
     name: 'card5',
     image: 'Icons/img-placeholder2.1.png',
     title: 'Functional Programming? YES',
     text: 'A daily selection of privately personalized reads; no accounts or sign-ups required; has been the industry\'s standard',
-    link: {'project': '', 'source': ''}
+    link: { project: '', source: '' },
+    buttons: {
+      langs: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+      other: [['See Live', ''], ['See Source', '']],
+    },
   },
   {
     name: 'card6',
     image: 'Icons/img-placeholder2.1.png',
     title: 'The Rabbit-Hole To End All Rabbit-Holes Just Got Deep!',
     text: 'A daily selection of privately personalized reads; no accounts or sign-ups required; has been the industry\'s standard',
-    link: {'project': '', 'source': ''}
+    link: { project: '', source: '' },
+    buttons: {
+      langs: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+      other: [['See Live', ''], ['See Source', '']],
+    },
   },
 ];
 
-let template = (obj) => {
-  let div = document.createElement('div');
+const template = (obj) => {
+  const div = document.createElement('div');
   div.classList.add('preview');
 
-  let divTop = document.createElement('div');
+  const divTop = document.createElement('div');
   divTop.classList.add('preview-divtop');
-  let h3 = document.createElement('h3');
+  const h3 = document.createElement('h3');
   h3.appendChild(document.createTextNode(obj.title));
   h3.classList.add('preview-title');
-  let divIcon = document.createElement('div');
-  let i = document.createElement('i');
+  const divIcon = document.createElement('div');
+  const i = document.createElement('i');
   i.style.height = '12px';
   i.classList.add('cross-menu2');
+  i.addEventListener('click', () => {
+    previewContainer.classList.toggle('active');
+  });
   divIcon.appendChild(i);
   divTop.appendChild(h3);
   divTop.appendChild(i);
 
-  let div1 = document.createElement('div');
-  for (let btn of buttons.langs) {
-    let button = document.createElement('button');
+  const div1 = document.createElement('div');
+  for (const btn of obj.buttons.langs) {
+    const button = document.createElement('button');
     button.appendChild(document.createTextNode(btn));
     div1.appendChild(button);
   }
   div1.classList.add('preview-langs');
 
-  let img = document.createElement('img');
+  const img = document.createElement('img');
   img.src = obj.image;
   img.alt = 'Showcase image.';
   img.classList.add('preview-img');
 
-  let p = document.createElement('p');
+  const p = document.createElement('p');
   p.appendChild(document.createTextNode(obj.text));
   p.classList.add('preview-text');
 
-  let div2 = document.createElement('div');
-  for (let btn of buttons.other) {
-    let button = document.createElement('button');
+  const div2 = document.createElement('div');
+  for (const btn of obj.buttons.other) {
+    const button = document.createElement('button');
     button.appendChild(document.createTextNode(btn[0]));
     button.setAttribute.source = btn[1];
     button.classList.add('see-project');
+    button.setAttribute('id', 'open-project');
     div2.appendChild(button);
   }
-  div2.classList.add('preview-buttons')
+  div2.classList.add('preview-buttons');
 
   div.appendChild(divTop);
   div.appendChild(div1);
   div.appendChild(img);
   div.appendChild(p);
   div.appendChild(div2);
-  
   return div;
-}
+};
 
-let templateFilled = template(cardsArr[2]);
-document.getElementById('preview-container').appendChild(templateFilled);
-
-document.querySelectorAll('.preview-container .preview-card').forEach(card => {
-  card.addEventListener('click', () => {
-    previewContainer.classList.add('active');
-    let name = card.getAttribute('data-name');
-    previewCard.forEach(preview => {
-      let target = preview.getAttribute('data-target');
-      if (name === target) {
-        let templateFilled = template(cardsArr[1]);
-        let div = document.getElementsByClassName('.preview-container');
-        div.appendChild(templateFilled);
+document.querySelectorAll('.grid-item-generic').forEach((card) => {
+  const openProject = card.querySelector('.open-project');
+  openProject.addEventListener('click', () => {
+    let selectedCard;
+    const name = card.getAttribute('data-name');
+    for (const card of cardsArr) {
+      if (card.name === name) {
+        selectedCard = card;
+        break;
       }
-    });
-  });
-});
-
-previewCard.forEach(card => {
-  card.querySelector('.cross-menu').addEventListener('click', () => {
-    card.classList.remove('active');
-    document.querySelector('.preview-container').classList.remove('active');
+    }
+    const templateFilled = template(selectedCard);
+    previewContainer.innerHTML = '';
+    previewContainer.appendChild(templateFilled);
+    previewContainer.classList.toggle('active');
   });
 });
