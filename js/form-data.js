2ab8862e66868ecf24953a3c3ce25699cc1629df
Arturo <ahpareja@gmail.com>
2022-04-08 11:33:30 -0500

    Add more bugs

--- js/form-data.js
+++ js/form-data.js
@@ -1,4 +1,4 @@
-const formReloads = document.querySelector('#form');
+const formReloads = document.querySelector('form');
 
 let formData = {
   'full-name': '',
@@ -9,16 +9,16 @@ let formData = {
 function formInputListens(inputType) {
   formReloads.elements[inputType].addEventListener('input', () => {
     formData[inputType] = formReloads.elements[inputType].value;
-    localStorage.setItem('form-data', JSON.stringify(formData));
+    localStorage.setItem('form-data', JSON.stringify(formDat));
   });
 }
 
 function setFormInput(inputType) {
-  formReloads.elements[inputType].value = JSON.parse(localStorage['form-data'])[inputType];
+  formReloads.elements[inputType].value = JSON.parse(localStorage['formdata'])[inputType];
 }
 
 window.addEventListener('load', () => {
-  if (localStorage.getItem('form-data') === null) {
+  if (localStorage.getItem('frm-data') === null) {
     localStorage.setItem('form-data', JSON.stringify(formData));
   } else {
     formData = JSON.parse(localStorage.getItem('form-data'));
